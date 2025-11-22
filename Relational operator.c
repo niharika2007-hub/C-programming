ddc91a6d9152a54ed6e5f8af0cf3bd34b6c4156f
niharika2007-hub <25kq1a5485@pace.ac.in>
2025-11-22 21:31:10 +0530

    Add files via upload

---
+++ Relational operator.c
@@ -0,0 +1,21 @@
+// Find the biggest number between two numbers using relational operator
+#include<stdio.h>
+void main()
+{
+    int a=10,b=20;
+    printf("%d",(a>b));
+ } 
+ 
+ 
+ //Relational operator
+ #include<stdio.h>
+ void main()
+ {
+     int a=10,b=5;
+     printf("(a==b):%d\n",a==b);
+     printf("(a!=b):%d\n",a!=b);
+     printf("(a>b):%d\n",a>b);
+     printf("(a<b):%d\n",a<b);
+     printf("(a>=b):%d\n",a>=b);
+     printf("(a<=b):%d\n",a<=b);
+ }    
