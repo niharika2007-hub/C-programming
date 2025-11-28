e0ec10b44f21fb555277731f3a84c94271650a5c
niharika2007-hub <25kq1a5485@pace.ac.in>
2025-11-28 08:40:28 +0530

    Add files via upload

---
+++ Conditional operator .c
@@ -0,0 +1,10 @@
+//write a program to print given number is even or odd
+#include<stdio.h>
+void main()
+{
+    int a;
+    a=105;
+    a%2==0? 
+    printf("even number"):printf("Odd number");
+ } 
+ 
