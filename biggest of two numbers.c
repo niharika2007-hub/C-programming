63f829ead5df20da1cdb00f03c3ca686211b365e
niharika2007-hub <25kq1a5485@pace.ac.in>
2025-11-28 08:43:22 +0530

    Add files via upload

---
+++ biggest of two numbers.c
@@ -0,0 +1,15 @@
+ //conditional operator for biggest of two numbers (if-else)
+ #include<stdio.h>
+ void main()
+ {
+     int a,b;
+     printf("enter two numbers");
+     scanf("%d%d",&a,&b);
+     if(a>b)
+     printf("a is biggest number");
+     else
+     printf("b is biggest number");
+ }    
+     
+
+ 
