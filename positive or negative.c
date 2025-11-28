976af1de49506463cab3d8d270d00abe5b0d93cf
niharika2007-hub <25kq1a5485@pace.ac.in>
2025-11-28 08:42:23 +0530

    Add files via upload

---
+++ positive or negative.c
@@ -0,0 +1,13 @@
+//conditional statement for positive or negative (if-else)
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter a value");
+    scanf("%d",a);
+    if(a>0)
+    printf("a is positive number");
+    else
+    printf("a is negative number");
+ } 
+ 
