166fcc280477484c4d38e3f71b7c0a91d1845500
niharika2007-hub <25kq1a5485@pace.ac.in>
2025-11-28 08:44:22 +0530

    Add files via upload

---
+++ if-else-if.c
@@ -0,0 +1,25 @@
+//selection statement to find the spelling of a number(if-else-if)
+#include<stdio.h>
+void main()
+{
+    int i;
+    printf("enter a number");
+    scanf("%d",&i);
+    if(i==1)
+    printf("One");
+    else if(i==2)
+    printf("Two");
+    else if(i==3)
+    printf("Three");
+    else if(i==4)
+    printf("Four");
+    else if(i==5)
+    printf("Five");
+    else
+    printf("Numbers out of limit");
+}  
+
+     
+
+ 
+    
