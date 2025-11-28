b364fb064d8cb56ceac85785b412b41975dd36f8
niharika2007-hub <25kq1a5485@pace.ac.in>
2025-11-28 08:39:54 +0530

    Add files via upload

---
+++ Relational operator.c
@@ -0,0 +1,12 @@
+ //Relational operator
+ #include<stdio.h>
+ void main()
+ {
+     int a=10,b=5;
+     printf("(a==b):%d\n",a==b);
+     printf("(a!=b):%d\n",a!=b);
+     printf("(a>b):%d\n",a>b);
+     printf("(a<b):%d\n",a<b);
+     printf("(a>=b):%d\n",a>=b);
+     printf("(a<=b):%d\n",a<=b);
+ }    
