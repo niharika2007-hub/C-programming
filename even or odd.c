2e312632a406c8b3b6d28aea743a41abe73a03a1
niharika2007-hub <25kq1a5485@pace.ac.in>
2025-11-28 08:42:55 +0530

    Add files via upload

---
+++ even or odd.c
@@ -0,0 +1,11 @@
+ //conditional statement for even and odd (if-else)
+ #include<stdio.h>
+ void main()
+ {
+     int a=80;
+     if(a%2==0)
+     printf("Even number");
+     else
+     printf("Odd number");
+ } 
+
